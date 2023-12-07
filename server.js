a125781e8c9de4bc95831e8f9cb1bed06275d823
ryang123ism <grantryan182@gmail.com>
2023-12-07 20:15:36 +0000

    updating server.js output

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ var host;
 var handleRequest = function(request, response) {
   response.setHeader('Content-Type', 'text/plain');
   response.writeHead(200);
-  response.write("DevOps Coursework 2!!! | Running on: ");
+  response.write("DevOps Coursework 2! | Running on: ");
   response.write(host);
-  response.end(" | v=1\n");
+  response.end(" | v=2\n");
   console.log("Running On:" ,host, "| Total Requests:", ++requests,"| App Uptime:", (new Date() - startTime)/1000 , "seconds", "| Log Time:",new Date());}
 
 var www = http.createServer(handleRequest);
